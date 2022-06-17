ebd79f8210bd064053ae9e115c5c4dde4c1672c5
Joao Gabriel <jgabriellima1067@gmail.com>
2022-06-16 21:17:22 -0300

    criando repository e service para categoria

--- src/main/java/com/joaogabriel/cursomc/repository/CategoriaRepository.java
+++ src/main/java/com/joaogabriel/cursomc/repository/CategoriaRepository.java
@@ -3,7 +3,5 @@ package com.joaogabriel.cursomc.repository;
 import com.joaogabriel.cursomc.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 }
--- src/main/java/com/joaogabriel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joaogabriel/cursomc/resources/CategoriaResource.java
@@ -1,54 +1,27 @@
 package com.joaogabriel.cursomc.resources;
 
-
 import com.joaogabriel.cursomc.domain.Categoria;
-import com.joaogabriel.cursomc.repository.CategoriaRepository;
+import com.joaogabriel.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+
 
 @RestController
-@RequestMapping(value="/categorias/")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    private CategoriaService service;
 
-    @GetMapping(value = "total")
-    public List<Categoria> listar() {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
 
-//        Categoria cat1 = new Categoria(1, "Informatica");
-//        Categoria cat2 = new Categoria(2,"Escritorio");
-//        List<Categoria> categoriaList = new ArrayList<>();
-//        categoriaList.add(cat1);
-//        categoriaList.add(cat2);
-        return categoriaRepository.findAll();
     }
-
-    @GetMapping("busca-categoria/{id}")
-    public Optional<Categoria> getByCategoriaId(@PathVariable Integer id) {
-        return categoriaRepository.findById(id);
-    }
-
-    @PostMapping(value = "salvar-categoria")
-    public Categoria salvarCategoria(@RequestBody Categoria categoria) {
-        return categoriaRepository.save(categoria);
-    }
-
-    @DeleteMapping("deleta-categoria/{id}")
-    public void deleteCategoria(@PathVariable Integer id) {
-        categoriaRepository.deleteById(id);
-    }
-
-    @PutMapping("atualiza-categoria/{id}")
-    public Categoria updateCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
-
-        Optional<Categoria> cat = categoriaRepository.findById(id);
-
-        cat.orElseThrow().setNome(categoria.getNome());
-        return categoriaRepository.save(cat.get());
-    }
-
 }
